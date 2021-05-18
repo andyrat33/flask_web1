01009adbfd2cd743cfcd2bbf678f4eb0bf61cacb
Andrew Ratcliffe <andrew.ratcliffe@nswcsystems.co.uk>
2021-05-18 20:32:17 +0100

    adds a new hello name route

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/hello/<string:name>')
+def hello(name: str):
+    return f'Hello {name}'
+
 
 if __name__ == '__main__':
     app.run()
